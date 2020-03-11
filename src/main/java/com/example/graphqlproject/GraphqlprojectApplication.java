30a38a389a6cc69c3241e31171c8bfcc98211e81
LionelBeato <lionel.beato@gmail.com>
2020-03-11 17:06:32 -0400

    first commit

---
+++ src/main/java/com/example/graphqlproject/GraphqlprojectApplication.java
@@ -0,0 +1,31 @@
+package com.example.graphqlproject;
+
+import graphql.Scalars;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLSchema;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GraphqlprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlprojectApplication.class, args);
+	}
+
+
+//	@Bean
+//	GraphQLSchema schema() {
+//		return GraphQLSchema.newSchema()
+//				.query(GraphQLObjectType.newObject()
+//						.name("query")
+//						.field(field -> field
+//								.name("test")
+//								.type(Scalars.GraphQLString)
+//								.dataFetcher(environment -> "response")
+//						)
+//						.build())
+//				.build();
+//	}
+}
